55d8fb28abd68cad0c8104539ac07c910896f12e
Joey Lapetina <joeylapetina@gmail.com>
2012-11-16 23:43:51 -0600

    heroku being booty - trying to fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Marubaru::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'yourhost.com' }
+  config.action_mailer.default_url_options = { :host => 'http://serene-brook-2691.herokuapp.com/' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
